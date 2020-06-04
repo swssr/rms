2f74b667075996f108ad27fbcfbd955da726c574
swssr <simocmo4@gmail.com>
2020-06-04 08:42:04 +0200

    add serve meter data as json

---
+++ rms-server/index.js
@@ -0,0 +1,20 @@
+/** Main imports  **/
+const express = require("express");
+const app = express();
+
+/** Other imports   */
+const cors = require("cors");
+
+/** Middleware   */
+app.use(cors());
+
+/** Import Services   */
+const MeterService = require("./meter.service");
+
+app.get("/api/data", async (req, res) => {
+  res.json((await MeterService.GetReadings()) || []);
+});
+
+/** Start server */
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Application started at port: ${PORT}`));
